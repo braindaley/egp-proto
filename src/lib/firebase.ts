a9779d72514eaa0d64d8a25124541ff21abfd5d5
Brian Daley <bdaley@goldencomm.com>
2025-07-30 12:23:43 +0000

    when i try to create an account

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "your_firebase_api_key",
+  authDomain: "your_firebase_auth_domain",
+  projectId: "your_firebase_project_id",
+  storageBucket: "your_firebase_storage_bucket",
+  messagingSenderId: "your_firebase_messaging_sender_id",
+  appId: "your_firebase_app_id",
 };
 
 let app: FirebaseApp;
